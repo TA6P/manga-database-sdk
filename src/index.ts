d7fbe872d10a3e06bb49f3796a32c4d22fb2b368
MMinusOne <michael.business.lb@proton.me>
2026-06-26 16:40:49 +0300

    intial

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import type { Chapter, MangaEntry, Page } from "./types";
+import ky, { type KyInstance } from "ky";
+
+export default class MangaDatabaseSDK {
+  private api: KyInstance;
+
+  constructor(
+    private readonly baseUrl: string,
+    private readonly timeout = 3_000,
+    private readonly retryLimit = 5,
+    private readonly backOffLimit = 3000,
+  ) {
+    this.api = ky.create({
+      baseUrl: this.baseUrl,
+      timeout: this.timeout,
+      retry: {
+        limit: this.retryLimit,
+        methods: ["get", "post"],
+        backoffLimit: this.backOffLimit,
+      },
+    });
+  }
+
+  async downloadPages(pageUrl: string): Promise<Uint8Array> {
+    const url = new URL(`https://atsu.moe/${pageUrl}`);
+
+    const response = await this.api.get(url).then((r) => r.bytes());
+
+    return response;
+  }
+
+  async fetchPages(chapterId: string): Promise<Page[]> {
+    const url = new URL(`/pages/${chapterId}`, this.baseUrl);
+
+    const response = await this.api
+      .get<{ data: Page[] }>(url)
+      .then((r) => r.json());
+
+    return response.data;
+  }
+
+  async fetchChapters(providerId: string): Promise<Chapter[]> {
+    const url = new URL(`/chapters/${mangaId}`, this.baseUrl);
+
+    const response = await this.api
+      .get<{ data: Chapter[] }>(url)
+      .then((r) => r.json());
+
+    return response.data;
+  }
+
+  async fetchProvider(mangaId: string) {
+    const url = new URL(`/best_provider/${mangaId}`, this.baseUrl);
+
+    const response = await this.api
+      .get<{ data: Chapter[] }>(url)
+      .then((r) => r.json());
+
+    return response.data;
+  }
+
+  async fetchTop({
+    sortBy,
+    page,
+    pageSize,
+  }: {
+    sortBy: string[];
+    page: number;
+    pageSize: number;
+  }): Promise<MangaEntry[]> {
+    const url = new URL("/top", this.baseUrl);
+
+    url.searchParams.set("sort_by", sortBy.join(","));
+    url.searchParams.set("page", page.toString());
+    url.searchParams.set("page_size", pageSize.toString());
+
+    const response = await this.api
+      .get<{ data: MangaEntry[] }>(url)
+      .then((r) => r.json());
+
+    return response.data;
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,49 @@
+export interface Page {
+  id: string;
+  number: number;
+  width: number;
+  height: number;
+  url: string;
+  aspectRatio: number;
+}
+
+export interface Chapter {
+  id: string;
+  chapterNumber: number;
+  title?: string;
+  fetchedAt: Date;
+}
+
+export interface MangaEntry {
+  id: string;
+  title: string;
+  type: string;
+  year?: number;
+  anime?: string;
+  rating?: number;
+  status?: string;
+  has_anime: boolean;
+  description?: string;
+  is_licensed: boolean;
+  native_title?: string;
+  content_rating: string;
+  total_chapters?: string;
+  last_updated_at: string;
+  romanized_title?: string;
+  popularity?: number;
+
+  action_score?: number;
+  romance_score?: number;
+  slice_of_life_score?: number;
+
+  tags: string[];
+  links: string[];
+  genres: string[];
+  authors: string[];
+  artists: string[];
+  end_date?: string;
+  start_date?: string;
+  end_date_is_estimated?: boolean;
+  start_date_is_estimated?: boolean;
+  cover?: any;
+}
